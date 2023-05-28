1c3cb2e9959a2afc50d0136d9cbf2486626f03ca
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-05-28 20:14:08 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,73 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import { decrement, increment } from "./Components/CounterSlice1";
-import { decrement_By_Twice, increment_By_Twice } from "./Components/CounterSice2";
-
+import {
+  decrement_By_Twice,
+  increment_By_Twice,
+} from "./Components/CounterSlice2";
 
 const App = () => {
   const dispatch = useDispatch();
   const StoreSelector = useSelector((state) => state.counterStore);
   const StoreSelector1 = useSelector((state) => state.counterStoreNew);
-  console.log("StoreSelector",StoreSelector)
-  console.log("StoreSelector",StoreSelector1)
-  return (
-    <div className="App">
-      <div>
-      {StoreSelector.count}
-      {StoreSelector.status}
-      <hr></hr>
-      <br></br>
-      {StoreSelector1.count}
-      {StoreSelector1.status}
-      <button
-        onClick={() => {dispatch(increment());}}>
-        Increment
-      </button>
-      <button
-        onClick={() => {dispatch(decrement());}}>
-        Decrement
-      </button>
-      </div>
-     <div>
-     <button
-        onClick={() => {dispatch(increment_By_Twice());}}>
-        Increment by 2
-      </button>
 
-      <button
-        onClick={() => {dispatch(decrement_By_Twice());}}>
-        Decrement by 2
-      </button>
-     </div>
+  return (
+    <div className="constainer">
+      <div className="container1">
+        <span
+          
+        >
+          {StoreSelector.count}
+        </span>
+        <span
+      
+        >
+          {StoreSelector.status}
+        </span>
+        <hr></hr>
+        <br></br>
+
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrement
+        </button>
+      </div>
+      <br></br>
+      <div className="constainer2"> 
+        <span >
+          {StoreSelector1.count}
+        </span>
+        <span>
+          {StoreSelector1.status}
+        </span>
+        <hr></hr>
+        <br></br>
+        <button
+            onClick={() => {
+            dispatch(increment_By_Twice());
+          }}
+        >
+          Increment by 2
+        </button>
+
+        <button
+          onClick={() => {
+            dispatch(decrement_By_Twice());
+          }}
+        >
+          Decrement by 2
+        </button>
+      </div>
     </div>
   );
 };
--- src/Components/ParentStore.js
+++ src/Components/ParentStore.js
@@ -1,6 +1,6 @@
 import { configureStore} from '@reduxjs/toolkit';
 import CounterSlice from './CounterSlice1';
-import CounterSliceNew from './CounterSice2';
+import CounterSliceNew from './CounterSlice2';
 
 const store = configureStore({
     reducer: {
